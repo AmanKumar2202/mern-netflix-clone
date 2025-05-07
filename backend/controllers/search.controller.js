0c04944df2e57b18a0517ba03f0d793901d5810b
AmanKumar2202 <amankumar220203@gmail.com>
2025-05-07 14:58:57 +0530

    initial commit

---
+++ backend/controllers/search.controller.js
@@ -0,0 +1,104 @@
+import { User } from "../models/user.model.js";
+import { fetchFromTMDB } from "../services/tmdb.service.js";
+
+export async function searchPerson(req,res){
+    const {query} = req.params;
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`)
+        if(response.results.length === 0){
+            return res.status(404).send(null);
+        }
+
+        await User.findByIdAndUpdate(req.user._id,{
+            $push:{
+                searchHistory:{
+                    id: response.results[0].id,
+                    image: response.results[0].profile_path,
+                    title: response.results[0].name,
+                    searchType:"person",
+                    createdAt: new Date(),
+                },
+            },
+        })
+        res.status(200).json({success: true, content: response.results});
+    } catch (error) {
+        console.log("Error in searchPerson controller: ",error.message);
+        res.status(500).json({success:false, message:"Internal Server Error"})
+    }
+}
+
+export async function searchMovie(req,res){
+    const {query} = req.params;
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`)
+        if(response.results.length === 0){
+            return res.status(404).send(null);
+        }
+
+        await User.findByIdAndUpdate(req.user._id,{
+            $push:{
+                searchHistory:{
+                    id: response.results[0].id,
+                    image: response.results[0].poster_path,
+                    title: response.results[0].title,
+                    searchType:"movie",
+                    createdAt: new Date(),
+                },
+            },
+        })
+        res.status(200).json({success:true, content: response.results})
+    } catch (error) {
+        console.log("Error in searchMovie controller: ",error.message);
+        res.status(500).json({success:false, message:"Internal Server Error"})
+    }
+}
+
+export async function searchTv(req,res){
+    const {query} = req.params;
+    try {
+        const response = await fetchFromTMDB(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=en-US&page=1`)
+        if(response.results.length === 0){
+            return res.status(404).send(null);
+        }
+
+        await User.findByIdAndUpdate(req.user._id,{
+            $push:{
+                searchHistory:{
+                    id: response.results[0].id,
+                    image: response.results[0].poster_path,
+                    title: response.results[0].name,
+                    searchType:"tv",
+                    createdAt: new Date(),
+                },
+            },
+        })
+        res.status(200).json({success:true, content: response.results})
+    } catch (error) {
+        console.log("Error in searchTv controller: ",error.message);
+        res.status(500).json({success:false, message:"Internal Server Error"})
+    }
+}
+
+export async function getSearchHistory(req,res){
+    try {
+        res.status(200).json({success:true, content:req.user.searchHistory});
+    } catch (error) {
+        res.status(500).json({success:false,message:"Internal Server Error"});
+    }
+}
+
+export async function removeItemFromSearchHistory(req,res){
+    let {id} = req.params;
+    id=parseInt(id);    //Since the id that is received is in the form of string and we cannot pull the string out so we have to convert it into integer through parseInt
+    try {
+        await User.findByIdAndUpdate(req.user._id,{
+            $pull:{     //used to remove an element and push is used to add an element
+                searchHistory:{id:id},
+            }
+        })
+        res.status(200).json({success:true,message:"Item removed from search history"})
+    } catch (error) {
+        console.log("Error in removeItemSearchHistory controller: ",error.message);
+        res.status(500).json({success:false, message:"Internal Server Error"})
+    }
+}
